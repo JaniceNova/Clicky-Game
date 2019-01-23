9c4786eded2482608c7e4c49904fa43471630c4b
JaniceNova <janicenovacek2016@gmail.com>
2019-01-23 08:55:22 -0800

    got it to return an id

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
 import React, { Component } from "react";
+
 import PicCard from "./components/PicCard";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
-import charaters from "./charaters.json";
+import char from "./charaters.json";
 
 class App extends Component {
   // Setting this.state.friends to the friends json array
   state = {
-    charaters: charaters,
+    randomId: 1,
+    score: 0,
+    tally: 0,
+    charaters: char,
     charatersGuessed: []
   };
-
+  componentDidMount() {
+    this.setNewRandomId(this.state.charaters);
+  }
+testfunc = id => {
+  alert ("the id returned is: "+id) 
+}
   pushCharater = id => {
-    // const charaters = this.state.charaters.filter(friend => friend.id !== id);
-    // this.setState({ charaters });
-    // Filter this.state.friends for friends with an id not equal to the id being removed
-    let charatersGuessed = this.state.charatersGuessed
-    charatersGuessed = this.state.charatersGuessed.push(this.id);
-    // Set this.state.friends equal to the new friends array
-    this.setState({ charatersGuessed: charatersGuessed });
-    console.log(charatersGuessed)
-  };
+    this.setState({
+      tally: this.state.tally + 1
+    });
+    if(this.state.randomId === id) {
+      this.setState({
+        score: this.state.score + 1
+      });
+      this.setNewRandomId(this.state.charaters);
+    }
+  }
+
+  setNewRandomId = (array) => {
+    const randomId = array[Math.floor(Math.random()*array.length)].id;
+    this.setState({
+      randomId: randomId
+    });
+  }
+
+  // pushCharater = (id) => {
+  //   // const charaters = this.state.charaters.filter(friend => friend.id !== id);
+  //   // this.setState({ charaters });
+  //   // Filter this.state.friends for friends with an id not equal to the id being removed
+  //   let charatersGuessed = this.state.charatersGuessed
+  //   charatersGuessed = this.state.charatersGuessed.push(this.id);
+  //   // Set this.state.friends equal to the new friends array
+  //   this.setState({ charatersGuessed: charatersGuessed });
+  //   console.log(charatersGuessed)
+  // };
 
   // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
@@ -37,6 +65,7 @@ Click on an image to earn points, but don't click on any more than once!</Title>
             image={friend.image}
             occupation={friend.occupation}
             location={friend.location}
+            testfunc={this.testfunc}
           />
         ))}
       </Wrapper>
